b5a3026d64756ba92da80f36d6af5111b16892de
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-03-15 13:52:39 +0100

    Versions refresh

--- src/main/java/com/example/Team.java
+++ src/main/java/com/example/Team.java
@@ -6,33 +6,4 @@ import jakarta.persistence.Id;
 
 
 @Entity
-public class Team {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String name;
-    private String stadium;
-
-    public Team(String name, String stadium) {
-        this.name = name;
-        this.stadium = stadium;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getStadium() {
-        return stadium;
-    }
-}
+public record Team(@Id @GeneratedValue Long id, String name, String stadium) { }
