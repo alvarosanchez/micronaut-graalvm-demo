43d26d2fc40052a5784f9c91d41a6c701a712e49
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-10-27 12:01:41 +0100

    Simplify repository

--- src/main/java/com/example/TeamRepository.java
+++ src/main/java/com/example/TeamRepository.java
@@ -4,7 +4,5 @@ import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
-public interface TeamRepository extends CrudRepository<Team, Long> {}
-
 @JdbcRepository(dialect = Dialect.POSTGRES)
-interface PostgresTeamRepository extends TeamRepository {}
+public interface TeamRepository extends CrudRepository<Team, Long> {}
