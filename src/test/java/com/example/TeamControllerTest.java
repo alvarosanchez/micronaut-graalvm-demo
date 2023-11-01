bed6b7fae5f3b6232ef254a0f65810cbe3941f6e
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-11-01 14:09:44 +0200

    Cleanup data after tests

--- src/test/java/com/example/TeamControllerTest.java
+++ src/test/java/com/example/TeamControllerTest.java
@@ -1,6 +1,5 @@
 package com.example;
 
-
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.annotation.Sql;
@@ -15,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 @Sql({"classpath:create.sql", "classpath:data.sql"})
+@Sql(value = "classpath:clean.sql", phase = Sql.Phase.AFTER_ALL)
 class TeamControllerTest {
 
     @Inject
