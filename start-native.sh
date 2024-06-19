e88d558a63cece8620f74e2d1fc1827d85599760
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-06-19 13:30:12 +0200

    Updates

--- start-native.sh
+++ start-native.sh
@@ -1,4 +1,4 @@
-#Run DB
+#Configure datasource
 export POSTGRES_PASSWORD="mysecretpassword123"
 export DATASOURCES_DEFAULT_URL="jdbc:postgresql://localhost:5432/postgres"
 export DATASOURCES_DEFAULT_USERNAME=postgres
@@ -6,4 +6,4 @@ export DATASOURCES_DEFAULT_PASSWORD=$POSTGRES_PASSWORD
 export DATASOURCES_DEFAULT_MIN_POOL_SIZE=1
 export DATASOURCES_DEFAULT_MAX_POOL_SIZE=10
 
-./target/teams-solution
+./teams-solution
