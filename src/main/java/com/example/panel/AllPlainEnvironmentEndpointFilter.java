48b2a29c14a8a5b8bc6f6b90f1de1b9219a65a92
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2024-09-19 10:47:59 +0200

    Main control panel

---
+++ src/main/java/com/example/panel/AllPlainEnvironmentEndpointFilter.java
@@ -0,0 +1,18 @@
+package com.example.panel;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.management.endpoint.env.EnvironmentEndpointFilter;
+import io.micronaut.management.endpoint.env.EnvironmentFilterSpecification;
+import jakarta.inject.Singleton;
+
+@Singleton
+@Requires(env = Environment.DEVELOPMENT)
+public class AllPlainEnvironmentEndpointFilter implements EnvironmentEndpointFilter {
+
+    @Override
+    public void specifyFiltering(@NonNull EnvironmentFilterSpecification specification) {
+        specification.legacyMasking();
+    }
+}
